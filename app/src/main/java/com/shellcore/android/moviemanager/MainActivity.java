6ff0ea36dcad0280e1d7fc423ef5d691dcaeda2a
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-06-14 13:02:14 -0500

    Se actualizaron las opciones del menu principal de la aplicación.

--- app/src/main/java/com/shellcore/android/moviemanager/MainActivity.java
+++ app/src/main/java/com/shellcore/android/moviemanager/MainActivity.java
@@ -80,18 +80,16 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id) {
+            case R.id.nav_now_playing:
 
-        } else if (id == R.id.nav_slideshow) {
+                break;
+            case R.id.nav_logout:
 
-        } else if (id == R.id.nav_manage) {
+                break;
+            case R.id.nav_upcoming:
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
