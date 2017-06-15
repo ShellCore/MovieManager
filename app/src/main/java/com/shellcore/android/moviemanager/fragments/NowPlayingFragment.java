a654d023a0f19efccf3c8464d8801a15136d6c27
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-06-15 09:51:36 -0500

    Se implementó la lista de películas por estrenar. Se agregó permisos de conexión a internet.

--- app/src/main/java/com/shellcore/android/moviemanager/fragments/NowPlayingFragment.java
+++ app/src/main/java/com/shellcore/android/moviemanager/fragments/NowPlayingFragment.java
@@ -40,8 +40,8 @@ public class NowPlayingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
+        // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_now_playing, container, false);
         ButterKnife.bind(this, v);
 
--- app/src/main/java/com/shellcore/android/moviemanager/model/Movie.java
+++ app/src/main/java/com/shellcore/android/moviemanager/model/Movie.java
@@ -65,7 +65,7 @@ public class Movie {
     }
 
     public String getPosterPath() {
-        return posterPath;
+        return "https://image.tmdb.org/t/p/w342" + posterPath;
     }
 
     public void setPosterPath(String posterPath) {
